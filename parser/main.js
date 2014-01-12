e9af89caceecfb127b2edf69403ff55a1d84fe1f
Alex Bardanov <dnbard@gmail.com>
2014-01-12 13:35:15 +0200

    fix for kinopoisk parser

--- parser/main.js
+++ parser/main.js
@@ -23,13 +23,20 @@ function parse(url, callback, failure){
 }
 
 function loadHtml(url, callback){
+    var options = {
+        url: url,
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36'
+        }
+    };
+
     var request = require('request');
-    request(url, function (error, response, body) {
+    request(options, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var $ = cheerio.load(body);
             callback($);
         }
-    })
+    }).end();
 }
 
 exports.parse = parse;
