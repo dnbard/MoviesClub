3e5dde6e99cac463ea7d4ee2aceb79184e8744fa
Alex Bardanov <dnbard@gmail.com>
2014-02-18 15:40:47 +0200

    Remove auth logic from static requests

--- bootstrap.js
+++ bootstrap.js
@@ -1,23 +1,37 @@
-var Users = require('./models/users.js');
+var Users = require('./models/users.js'),
+    utils = require('./utils.js');
 
+//TODO: refactor this part
+function isStaticContent(url){
+    var staticContentTokens = ['javascripts', 'stylesheets', 'images', 'fonts'];
 
+    for(var i = 0; i < staticContentTokens.length; i++){
+        var staticToken = staticContentTokens[i];
+        if (utils.stringContains(url, staticToken)) return true;
+    }
+    return false;
+}
 
 var userAuth = function(req, res, next){
     var authFailure = function(){
         res.locals.auth = false;
+        res.locals.user = null;
         next();
     };
 
-	var userToken = req.cookies.uid;
-	Users.FindByToken(userToken, function(user){
-        if (user.length > 0){
-            res.locals.user = user[0];
-            res.locals.auth = true;
-            next();
-        } else authFailure();
-    }, function(){
-        authFailure()
-    });
+    if (isStaticContent((req.url))) authFailure();
+    else {
+        var userToken = req.cookies.uid;
+        Users.FindByToken(userToken, function(user){
+            if (user.length > 0){
+                res.locals.user = user[0];
+                res.locals.auth = true;
+                next();
+            } else authFailure();
+        }, function(){
+            authFailure()
+        });
+    }
 }
 
 exports.userAuth = userAuth;
